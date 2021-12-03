60d9e1d8688f3e4570d0a6f3eacf1d2fce7e150a
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-12-03 16:46:08 +0100

    adds new istance as a prime user

--- index.php
+++ index.php
@@ -6,11 +6,14 @@
         -prodotto     
 */
 
-include "data/dataUser.php";
 include "classes/User.php"; 
+include "classes/UserPrime.php"; 
 
-$pippo = new User("marco", "magno", "marco.magno@html.co", "3334445566");
+$pippo = new User("marco", "magno", "marco.magno@html.co", "3334445566", "32");
 var_dump($pippo);
+
+$pluto = new UserPrime("luigi", "magno", "luigi.magno@html.co", "4445556677", "40");
+var_dump($pluto)
 
 
 // echo $pippo->nome;
