f3266ec65c9c7bdc86ffb16d8991b64eb1a5f0b2
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-12-03 16:34:07 +0100

    adds phone number

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 include "data/dataUser.php";
 include "classes/User.php"; 
 
-$pippo = new User("marco", "magno", "marco.magno@gmail.co");
+$pippo = new User("marco", "magno", "marco.magno@html.co", "3334445566");
 var_dump($pippo);
 
 
