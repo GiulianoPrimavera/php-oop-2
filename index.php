aa8d5d27c95d3196d1c09f5a1c84dc4bdeaf6312
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-12-03 16:54:33 +0100

    adds istance for the class products

--- index.php
+++ index.php
@@ -8,14 +8,16 @@
 
 include "classes/User.php"; 
 include "classes/UserPrime.php"; 
+include "classes/Product.php"; 
 
 $pippo = new User("marco", "magno", "marco.magno@html.co", "3334445566", "32");
 var_dump($pippo);
 
 $pluto = new UserPrime("luigi", "magno", "luigi.magno@html.co", "4445556677", "40");
-var_dump($pluto)
+var_dump($pluto);
 
-
+$lavatrice = new Product("lavatrice2000", "2000", "lavatrice grande");
+var_dump($lavatrice)
 // echo $pippo->nome;
 
 
