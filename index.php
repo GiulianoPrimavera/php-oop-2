1d1deabad8e1677e67c06bf9017fb899d2ea1e03
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-12-04 17:18:02 +0100

    adds a different type of product class

--- index.php
+++ index.php
@@ -9,16 +9,21 @@
 include "classes/User.php"; 
 include "classes/UserPrime.php"; 
 include "classes/Product.php"; 
+include "classes/ProductFood.php"; 
 
 $pippo = new User("marco", "magno", "marco.magno@html.co", "3334445566", "32");
 var_dump($pippo);
 
 $pluto = new UserPrime("luigi", "magno", "luigi.magno@html.co", "4445556677", "40");
+$pluto->setCodice(40);
 var_dump($pluto);
 
-$lavatrice = new Product("lavatrice2000", "2000", "lavatrice grande");
-var_dump($lavatrice)
-// echo $pippo->nome;
+$lavatrice = new Product("lavatrice2000", "2000", "lavatrice grande", "20");
+var_dump($lavatrice);
+
+$sushi = new ProductFood("sushi", "20", "sushi rolls", "10");
+$sushi->setFoodType("not vegan");
+var_dump($sushi);
 
 
 ?>
