77960503b85a99546cb60616c8c5dae920a9dd56
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-12-10 17:19:19 +0100

    adds the cart as a trait and a function that adds a product to a cart

--- traits/Cart.php
+++ traits/Cart.php
@@ -4,11 +4,19 @@ trait Cart {
     protected $cart = [];
 
     //funzione che gestisce i pagamenti
-    public function buyProduct($prodotto){
-        $this->cart[] = $prodotto;
-        $this->setNewCredit($prodotto->prezzo);
+    public function addProduct($prodotto){
+        if(array_key_exists($prodotto->getNome(), $this->cart)){
+            $this->cart[$prodotto->getNome()]["quantita"]++;
+        }else{
+            $this->cart[$prodotto->getNome()] = ["prodotto" => $prodotto, "quantita" => 1];
+        }
+        // $this->setNewCredit($prodotto->prezzo);
     }
 
+    //funzione remove product
+
+    //funzione calcola prezzo
+    
 }
 
 ?>
